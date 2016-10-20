5b1cfa9b2162466a9ad17e80cdb1afe81b5c3c07
sitiaisyah1019 <sitimessi10@gmail.com>
2016-10-20 11:51:57 +0700

    Latihan_2_2

---
+++ Selasa_141113678_SitiAisyah/Latihan_2_1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Latihan_2_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bulan = new System.Windows.Forms.DomainUpDown();
+            this.tanggal = new System.Windows.Forms.NumericUpDown();
+            this.hapus = new System.Windows.Forms.Button();
+            this.tambah = new System.Windows.Forms.Button();
+            this.kalender = new System.Windows.Forms.MonthCalendar();
+            ((System.ComponentModel.ISupportInitialize)(this.tanggal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bulan
+            // 
+            this.bulan.Items.Add("Januari");
+            this.bulan.Items.Add("Februari");
+            this.bulan.Items.Add("Maret");
+            this.bulan.Items.Add("April");
+            this.bulan.Items.Add("Mei");
+            this.bulan.Items.Add("Juni");
+            this.bulan.Items.Add("Juli");
+            this.bulan.Items.Add("Agustus");
+            this.bulan.Items.Add("September");
+            this.bulan.Items.Add("Oktober");
+            this.bulan.Items.Add("November");
+            this.bulan.Items.Add("Desember");
+            this.bulan.Location = new System.Drawing.Point(181, 22);
+            this.bulan.Name = "bulan";
+            this.bulan.Size = new System.Drawing.Size(120, 20);
+            this.bulan.TabIndex = 11;
+            this.bulan.SelectedItemChanged += new System.EventHandler(this.bulan_SelectedItemChanged);
+            // 
+            // tanggal
+            // 
+            this.tanggal.Location = new System.Drawing.Point(9, 22);
+            this.tanggal.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.tanggal.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.tanggal.Name = "tanggal";
+            this.tanggal.Size = new System.Drawing.Size(120, 20);
+            this.tanggal.TabIndex = 7;
+            this.tanggal.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // hapus
+            // 
+            this.hapus.Location = new System.Drawing.Point(630, 16);
+            this.hapus.Name = "hapus";
+            this.hapus.Size = new System.Drawing.Size(75, 23);
+            this.hapus.TabIndex = 10;
+            this.hapus.Text = "Hapus";
+            this.hapus.UseVisualStyleBackColor = true;
+            this.hapus.Click += new System.EventHandler(this.hapus_Click);
+            // 
+            // tambah
+            // 
+            this.tambah.Location = new System.Drawing.Point(521, 16);
+            this.tambah.Name = "tambah";
+            this.tambah.Size = new System.Drawing.Size(75, 23);
+            this.tambah.TabIndex = 10;
+            this.tambah.Text = "Tambah";
+            this.tambah.UseVisualStyleBackColor = true;
+            this.tambah.Click += new System.EventHandler(this.tambah_Click);
+            // 
+            // kalender
+            // 
+            this.kalender.CalendarDimensions = new System.Drawing.Size(2, 2);
+            this.kalender.Location = new System.Drawing.Point(9, 63);
+            this.kalender.Name = "kalender";
+            this.kalender.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 389);
+            this.Controls.Add(this.bulan);
+            this.Controls.Add(this.tanggal);
+            this.Controls.Add(this.hapus);
+            this.Controls.Add(this.tambah);
+            this.Controls.Add(this.kalender);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tanggal)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DomainUpDown bulan;
+        private System.Windows.Forms.NumericUpDown tanggal;
+        private System.Windows.Forms.Button hapus;
+        private System.Windows.Forms.Button tambah;
+        private System.Windows.Forms.MonthCalendar kalender;
+    }
+}
+
---
+++ Selasa_141113678_SitiAisyah/Latihan_2_1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Latihan_2_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            bulan.SelectedItem = "April";
+
+            DateTime awal = new DateTime(2010, 1, 1);
+            DateTime akhir = new DateTime(2018, 12, 31);
+            TimeSpan jarak = akhir - awal;
+            int hari = jarak.Days;
+            for (var i = 0; i <= hari; i++)
+            {
+                var bold = awal.AddDays(i);
+                switch (bold.DayOfWeek)
+                {
+                    case DayOfWeek.Wednesday:
+                        kalender.AddBoldedDate(bold);
+                        break;
+                    case DayOfWeek.Thursday:
+                        kalender.AddBoldedDate(bold);
+                        break;
+                }
+
+            }
+            kalender.AddAnnuallyBoldedDate(new DateTime(1996, 04, 05));
+        }
+
+        private void tambah_Click(object sender, EventArgs e)
+        {
+            kalender.AddAnnuallyBoldedDate(new DateTime(2016, bulan.SelectedIndex + 1, Convert.ToInt32(tanggal.Value)));
+            kalender.UpdateBoldedDates();
+        }
+
+        private void hapus_Click(object sender, EventArgs e)
+        {
+            kalender.RemoveAnnuallyBoldedDate(new DateTime(2016, bulan.SelectedIndex + 1, Convert.ToInt32(tanggal.Value)));
+            kalender.UpdateBoldedDates();
+        }
+
+        private void bulan_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
