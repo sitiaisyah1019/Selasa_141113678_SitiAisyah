38b531d39a547ec568fe6a5a9960d8d878b02f31
sitiaisyah1019 <sitimessi10@gmail.com>
2016-11-26 18:34:01 +0700

    VolleyBall Problem

---
+++ VolleyBall Problem/VolleyBall Problem/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace VolleyBall_Problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Hitung = new System.Windows.Forms.Button();
+            this.Hasil = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Teks2 = new System.Windows.Forms.TextBox();
+            this.Teks1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Hitung
+            // 
+            this.Hitung.Location = new System.Drawing.Point(122, 94);
+            this.Hitung.Name = "Hitung";
+            this.Hitung.Size = new System.Drawing.Size(75, 23);
+            this.Hitung.TabIndex = 13;
+            this.Hitung.Text = "Hitung";
+            this.Hitung.UseVisualStyleBackColor = true;
+            this.Hitung.Click += new System.EventHandler(this.Hitung_Click);
+            // 
+            // Hasil
+            // 
+            this.Hasil.Enabled = false;
+            this.Hasil.Location = new System.Drawing.Point(80, 151);
+            this.Hasil.Name = "Hasil";
+            this.Hasil.Size = new System.Drawing.Size(167, 20);
+            this.Hasil.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Hasil";
+            // 
+            // Teks2
+            // 
+            this.Teks2.Location = new System.Drawing.Point(80, 57);
+            this.Teks2.Name = "Teks2";
+            this.Teks2.Size = new System.Drawing.Size(167, 20);
+            this.Teks2.TabIndex = 10;
+            // 
+            // Teks1
+            // 
+            this.Teks1.Location = new System.Drawing.Point(80, 12);
+            this.Teks1.Name = "Teks1";
+            this.Teks1.Size = new System.Drawing.Size(167, 20);
+            this.Teks1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Skor B";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Skor A";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 193);
+            this.Controls.Add(this.Hitung);
+            this.Controls.Add(this.Hasil);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Teks2);
+            this.Controls.Add(this.Teks1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Hitung;
+        private System.Windows.Forms.TextBox Hasil;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Teks2;
+        private System.Windows.Forms.TextBox Teks1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ VolleyBall Problem/VolleyBall Problem/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VolleyBall_Problem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const long MOD = 1000000007;
+        private void Hitung_Click(object sender, EventArgs e)
+        {
+            long a, b, hasil;
+            a = Convert.ToInt64(Teks1.Text);
+            b = Convert.ToInt64(Teks2.Text);
+            if (a < b)
+            {
+                long temp = a;
+                a = b;
+                b = temp;
+            }
+
+            if ((a > 25 && a - b != 2) || (a - b < 2) || (a < 25))
+            {
+                hasil = 0;
+            }
+            else
+            {
+                hasil = kombinasi(Math.Min(a + b - 1, 47), Math.Min(a - 1, 24));
+                hasil *= powerMod(2, a - 25);
+                hasil %= MOD;
+            }
+            Hasil.Text = hasil.ToString();
+        }
+
+        private long kombinasi(long n, long r)
+        {
+            if (n < r) return 0;
+            long hasil = 1;
+            hasil *= faktorial(n);
+            hasil %= MOD;
+            hasil *= InverseEuler(faktorial(n - r));
+            hasil %= MOD;
+            hasil *= InverseEuler(faktorial(r));
+            hasil %= MOD;
+            return hasil;
+        }
+
+        private long InverseEuler(long n)
+        {
+            return powerMod(n, MOD - 2);
+        }
+
+        private long faktorial(long n)
+        {
+            long hasil = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                hasil *= i;
+                hasil %= MOD;
+            }
+            return hasil;
+        }
+
+        private long powerMod(long b, long e)
+        {
+            long res = 1;
+            while (e > 0)
+            {
+                if (e % 2 == 1)
+                {
+                    res = (res * b) % MOD;
+                }
+                b = (b * b) % MOD;
+                e /= 2;
+            }
+            return res % MOD;
+        }
+    }
+}
